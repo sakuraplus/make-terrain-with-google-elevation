e523ec313447d03c8c1085fc3d1091da10d864dd
sakusaku <sakuraplus@163.com>
2017-04-18 11:21:30 +0800

    add editor

---
+++ Assets/Scripts/MakeTerrainEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor; // this is needed since this script references the Unity Editor
+
+[CustomEditor(typeof(main ))]
+public class MakeTerrainEditor : Editor { // extend the Editor class
+
+	// called when Unity Editor Inspector is updated
+	public override void OnInspectorGUI()
+	{
+		// show the default inspector stuff for this component
+		DrawDefaultInspector();
+
+		// get a reference to the GameManager script on this target gameObject
+		main _main = (main)target;
+
+		// add a custom button to the Inspector component
+		if(GUILayout.Button("run"))
+		{
+			// if button pressed, then call function in script
+			_main.EditorCall ();
+		}
+
+	
+
+
+		// //////////////////////////
+//		if(GUILayout.Button("print"))
+//		{
+//			// if button pressed, then call function in script
+//			Debug.Log ("oooo");
+//			if (EditorUtility.DisplayDialog("????",   "aaaaa?",   "Yes00", "No")) {
+//		
+//			}
+//		}
+
+	}
+}
