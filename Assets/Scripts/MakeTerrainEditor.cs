23b11c41d1024ebc75d6a0b08b1ef1861ecb1d14
saku <sakuraplus@163.com>
2017-04-20 02:14:18 +0800

    增加根据真实比例绘制高度的功能

--- Assets/Scripts/MakeTerrainEditor.cs
+++ Assets/Scripts/MakeTerrainEditor.cs
@@ -14,17 +14,25 @@ public class MakeTerrainEditor : Editor { // extend the Editor class
 		// get a reference to the GameManager script on this target gameObject
 		main _main = (main)target;
 
-
+		GUILayout.Label ("mesh size="+_main.size.ToString () );
 
 		// add a custom button to the Inspector component
 		if(GUILayout.Button("combine meshs"))
 		{
 			_main.meshcombine ();//EditorCall ();//editor 不调用协程
 		}
-		if(GUILayout.Button("save a prefab"))
+		if(GUILayout.Button("save as prefab"))
 		{
 			_main.saveprefab  ();
 		}
-
+		if(GUILayout.Button("save as mesh"))
+		{
+			_main.savemesh   ();
+		}
+		GUILayout.Label ("test" );
+		if(GUILayout.Button("calc size"))
+		{
+			_main.size=_main.calcMeshSize(_main.sizemesh);
+		}
 	}
 }
--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -11,36 +11,56 @@ public class main : MonoBehaviour {
 
     GameObject terrmanager;//= new GameObject();
     GameObject[] arrTrr;//= new GameObject[9];
-	[SerializeField,HeaderAttribute ("latitude and longitude of the northwest")]
+
+	[Header("latitude and longitude of the northwest")]
 	[Range (-90,90)]
 	public   float lat = 30;			//起点纬度，北极90，南极-90
 	[Range (-180,180)]
 	public   float lng = 70;			//起点经度，英国东方为正，应该西方为负
 
-	[SerializeField,HeaderAttribute ("latitude and longitude of the southeast")]
+	[Header ("latitude and longitude of the southeast")]
 	[Range (-90,90)]
 	public   float endlat = 20;			//终点纬度
 	[Range (-180,180)]
 	public   float endlng = 90;			//终点经度
 
-	[SerializeField,HeaderAttribute ("size of the mesh")]
-	public Vector2 size = new Vector2 (100, 100);
 
-	[SerializeField,HeaderAttribute ("segment one mesh block in  lng,lat")]
-	public Vector2 segment=new Vector2(5,5);//每块地图分段数
-	[SerializeField,HeaderAttribute ("Default material of each block")]
+
+	//[SerializeField,HeaderAttribute ("Default material of each block")]
 	[HideInInspector]
-	public Material matTrr;			//地形预设材质
-	[SerializeField,HeaderAttribute ("Get a ELE KEY at developers.google.com/maps/documentation/elevation")]
+	public Material matTrr;	
+	//地形预设材质
+
+	[SerializeField,Header ("ELE KEY ")]
+	[Tooltip("Get a ELE KEY at developers.google.com/maps/documentation/elevation")]
 	string  googleELEAPIKey="";
-	[SerializeField,HeaderAttribute ("Get a STM KEY at ")]
+	[SerializeField,Header ("Get a STM KEY at ")]
 	string  googleSTMAPIKey="";
+	[Space(20)]
 	public static string ELEAPIkey;
 	public static string STMAPIkey;
 	//https://developers.google.com/maps/documentation/elevation/start?
-    const float earthR = 100;//地球半径
+
+	[SerializeField,HeaderAttribute ("segment one mesh block in  lng,lat")]
+	public Vector2 segment=new Vector2(5,5);//每块地图分段数
+
+	[Header( "size of the mesh in lat")]
+	public float sizemesh=100;
+
+//	[SerializeField,  HeaderAttribute ("size of the mesh")]
+	[HideInInspector]
+	public  Vector3 size = new Vector3 (100, 100,1);
+	[SerializeField,Header("the addition of real height data")]
+	[Tooltip("1 means the real scale")]
+	[Range (0.01f,1000f)]
+	float heightScale=1f;
+
+
+    const float earthR = 6371000;//地球半径
 	[SerializeField]
 	bool _havelicense=false;
+
+
 
     void Start () {
 		
@@ -117,17 +137,17 @@ public class main : MonoBehaviour {
 			Debug.LogWarning ("you need ele key");
 			return;
 		}
-
+		if ((lat == endlat) || (lng == endlng)) {
+			Debug.LogWarning ("incorrect geographical coordinate");
+			return;
+		}
 
 		terrmanager = new GameObject();
 		arrTrr = new GameObject[9];
 
 		terrmanager.name = "TRRMAG";
 
-		if ((lat == endlat) || (lng == endlng)) {
-			Debug.LogWarning ("incorrect geographical coordinate");
-			return;
-		}
+	
 		Trimlatlng ();//处理输入的经纬度信息，保证为西北，东南两点
 
 		//每个分块纬度差
@@ -141,20 +161,7 @@ public class main : MonoBehaviour {
 			steplng = (endlng - lng) / 3;
 		}
 
-
-		//=(endlng-lng)/3;// (float)Math.Floor(steplng*10)/10;
-		//计算每个地图块终点经纬度
-
-
-		float ttt=(endlat+lat)/2;
-		ttt = Mathf.PI * ttt / 180;//
-		//print ("endlat-lat)/2=" + ttt+ "   cos (ttt)="+Mathf.Cos (ttt)+"  ///"+Mathf.Acos(ttt));
-		ttt= size.x * Mathf.Abs (Mathf.Cos(ttt ));
-		print ("Math.Abs (Math.Cos(ttt ))" + ttt);
-		size.y = ttt * Mathf.Abs (steplng / steplat);;
-
-		print("steplat=" + steplat + "  steplng=" + steplng+" size="+size );//steplat0.5729578steplng3.71444
-
+		size = calcMeshSize (sizemesh);//以纬度方向size y计算经度方向距离x
 
 
 		////////////////////////////////
@@ -186,20 +193,62 @@ public class main : MonoBehaviour {
 			arrTrr [i].transform.parent = terrmanager.transform;
 		}
 
-		arrTrr[0].transform.Translate(new Vector3(-1*size.y, -50, size.x));
-		arrTrr[1].transform.Translate(new Vector3(-1*size.y, -50, 0));
-		arrTrr[2].transform.Translate(new Vector3(-1*size.y , -50, -1*size.x));
-		arrTrr[3].transform.Translate(new Vector3(0		, -50, size.x));
+		arrTrr[0].transform.Translate(new Vector3(-1*size.x, -50, size.z));
+		arrTrr[1].transform.Translate(new Vector3(-1*size.x, -50, 0));
+		arrTrr[2].transform.Translate(new Vector3(-1*size.x , -50, -1*size.z));
+		arrTrr[3].transform.Translate(new Vector3(0		, -50, size.z));
 		arrTrr[4].transform.Translate(new Vector3(0		, -50, 0));
-		arrTrr[5].transform.Translate(new Vector3(0		 , -50,-1*size.x));
-		arrTrr[6].transform.Translate(new Vector3(size.y	, -50, size.x));
-		arrTrr[7].transform.Translate(new Vector3(size.y	, -50 ,0));
-		arrTrr[8].transform.Translate(new Vector3(size.y	 , -50,-1*size.x));
+		arrTrr[5].transform.Translate(new Vector3(0		 , -50,-1*size.z));
+		arrTrr[6].transform.Translate(new Vector3(size.x	, -50, size.z));
+		arrTrr[7].transform.Translate(new Vector3(size.x	, -50 ,0));
+		arrTrr[8].transform.Translate(new Vector3(size.x	 , -50,-1*size.z));
 
 
 
 
 	}
+
+
+	public Vector3 calcMeshSize(float sizelat)
+	{
+		Vector3 size;
+		size.z = sizelat;
+		float	steplatall=Mathf.Abs (endlat-lat); //(float)Math.Floor(steplat*10)/10;
+		//每个分块经度差
+		//经度差绝对值>180时，取endlng+360计算step.计算后经度超过180的部分在索取数据时处理
+		float	steplngall;
+		if (Math.Abs (endlng - lng) > 180) {
+			steplngall = (360 + endlng - lng) ;
+		} else {
+			steplngall = (endlng - lng) ;
+		}
+
+		float ttt=(endlat+lat)/2;//区域的平均纬度
+		ttt =Mathf.Deg2Rad*ttt;// 角度转弧度=Mathf.PI * ttt / 180;//
+		//print ("endlat-lat)/2=" + ttt+ "   cos (ttt)="+Mathf.Cos (ttt)+"  ///"+Mathf.Acos(ttt));
+		ttt=  Mathf.Abs (Mathf.Cos(ttt ));//当前纬度下，1纬度与1经度之间的距离比
+		print ("Math.Abs (Math.Cos(ttt ))" + ttt);
+		size.x =size.z * ttt * Mathf.Abs (steplngall / steplatall);//根据当前纬度下跨越的纬度与跨越的经度距离的比例关系，求lng方向的mesh尺寸
+
+		float distancelat = 2 * Mathf.PI * earthR * steplatall / 360;//计算纬度方向实际距离
+		float _scale = size.z / distancelat;//单位实际距离对应的mesh大小
+	
+		size.y=_scale*heightScale ;
+		print("distancelat="+distancelat+"  _add="+_scale+"  sizey="+size.y);
+		return size;
+		//print("steplat=" + steplat + "  steplng=" + steplng+" size="+size );//steplat0.5729578steplng3.71444
+	}
+
+
+
+//	public float RefreshHeight()
+//	{
+//		for (int i = 0; i < arrTrr.Length; i++) {
+//		arrTrr[i].
+//		}
+//	}
+
+
 	GameObject _newmeshobj;
 	public void meshcombine()
 	{
@@ -239,18 +288,6 @@ public class main : MonoBehaviour {
 			//为合并后的新Mesh指定材质 ------------------------------  
 			_newmeshobj.GetComponent<MeshRenderer>().sharedMaterials = _materials;   
 
-			//////////////////////////////保存一个mesh
-			string baseResultFolder="Assets/";
-			string  dateStr = DateTime.Now.ToString("yyyy-MM-dd HH-mm");
-			baseResultFolder += dateStr;
-			if (!Directory.Exists(baseResultFolder)) 
-			{
-				Directory.CreateDirectory(baseResultFolder);
-			}
-			string strfilename=baseResultFolder+"/aaa" +".asset";
-			Mesh m1 = _newmeshobj . GetComponent<MeshFilter>().mesh;  
-			AssetDatabase.CreateAsset(m1, strfilename);  
-			//////////////////////////////////end 保存一个mesh
 
 		} else {
 			Debug.LogWarning ("run first！");
@@ -259,11 +296,34 @@ public class main : MonoBehaviour {
 	public void saveprefab()
 	{
 		print ("save pfb" + _newmeshobj);
-		print ("save pfb" + GameObject.Find("CombindeMesh"));
-		if (GameObject.Find("CombindeMesh")) {
+		print ("save pfb" + "Assets/savePrefab"+DateTime.Now.ToString("MM-dd HH-mm")+".prefab");
+		if (_newmeshobj) {
 			print ("saving");
-			PrefabUtility.CreatePrefab ("Assets/xxx2.prefab", GameObject.Find("CombindeMesh"), ReplacePrefabOptions.ReplaceNameBased);
+			string savefilepath="Assets/savePrefab"+DateTime.Now.ToString("MM-dd HH-mm")+".prefab";
+			PrefabUtility.CreatePrefab (savefilepath, GameObject.Find("CombindeMesh"), ReplacePrefabOptions.ReplaceNameBased);
+		}else{
+			Debug.LogWarning ("obj not found,run first");
+
 		}
 	}
+	public void savemesh()
+	{
+		if (_newmeshobj) {
+					//////////////////////////////保存一个mesh
+					string baseResultFolder="Assets/savemesh";
+					string  dateStr = DateTime.Now.ToString("yyyy-MM-dd HH-mm");
+					baseResultFolder += dateStr;
+					if (!Directory.Exists(baseResultFolder)) 
+					{
+						Directory.CreateDirectory(baseResultFolder);
+					}
+					string strfilename=baseResultFolder+"/aaa" +".asset";
+					Mesh m1 = _newmeshobj . GetComponent<MeshFilter>().mesh;  
+					AssetDatabase.CreateAsset(m1, strfilename);  
+					//////////////////////////////////end 保存一个mesh
+		}else{
+			Debug.LogWarning ("obj not found,run first");
 
+		}
+	}
 }
